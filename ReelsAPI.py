1124af6f90e38c66fe17e758ec25a264125ee683
浪人 <mighxi@yahoo.com>
2022-03-29 18:17:38 +0300

    v0.1

--- ReelsAPI.py
+++ ReelsAPI.py
@@ -8,6 +8,3 @@ class Insta:
         fetch_id = self.gram.media_pk_from_url(url)
         info = self.gram.media_info(fetch_id).dict()
         return info['video_url']
-
-
-#print(Insta().VideoURL('https://www.instagram.com/p/CbiU611JZ3i/'))
