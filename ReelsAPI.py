09862bb2b5d22b06ebdc03ac33d2e00b1518f07f
浪人 <mighxi@yahoo.com>
2022-04-30 23:19:16 +0300

    v0.2

--- ReelsAPI.py
+++ ReelsAPI.py
@@ -1,10 +1,17 @@
 from instagrapi import Client
+from instagrapi import exceptions
+
+class LoginError(Exception):
+    pass
 
 class Insta:
     def __init__(self):
         self.gram = Client()
 
     def VideoURL(self, url):
-        fetch_id = self.gram.media_pk_from_url(url)
-        info = self.gram.media_info(fetch_id).dict()
-        return info['video_url']
+        try:
+            fetch_id = self.gram.media_pk_from_url(url)
+            info = self.gram.media_info_a1(fetch_id).dict()
+            return info['video_url']
+        except exceptions.LoginRequired:
+            raise LoginError('Sorry, can\'t access this post without logging in.')
